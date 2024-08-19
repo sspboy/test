e10b8b205226e921033ac15046b6accc68bfca83
ssp <sspboy@163.com>
2024-08-19 21:34:21 +0800

    'home'

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,8 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://192.168.3.120:5000', // 你的后端服务地址
+//        target: 'http://192.168.2.106:5000', // 你的后端服务地址
+        target: 'http://192.168.2.106:5000', // 你的后端服务地址
         changeOrigin: true,
         pathRewrite: {
           '^/api': '', // 重写路径，去掉 /api 前缀
