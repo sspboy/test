06bb8c3c799f20069041d5f167f189792f380e16
sspboy <sspboy@163.com>
2025-07-11 22:24:49 +0800

    '20250711home'

--- src/components/AppMarket/Douyinshop/ProductList/more_select.vue
+++ src/components/AppMarket/Douyinshop/ProductList/more_select.vue
@@ -4,13 +4,37 @@
             <a-button style="margin-right: 8px ;font-size:12px;" size="small" @click="onClose" >取消</a-button>
             <a-button type="primary" size="small" style="font-size: 12px;" @click="onClose">查询</a-button>
         </template>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <a-layout-content style="padding: 0 100px;background-color: aqua;">
+            <a-row>
+                <a-col :span="6">
+                    <a-textarea 
+                    v-model:value="SelectData.product_id_list" 
+                    placeholder="多个商品ID查询 支持100个 逗号隔开" 
+                    size="small" 
+                    />
+                </a-col>
+                <a-col :span="6">
+                    <a-textarea 
+                        v-model:value="SelectData.sku_code" 
+                        placeholder="多个商品编码查询 支持50个 逗号隔开" 
+                        size="small" 
+                    />
+                </a-col>
+                <a-col :span="6"><a-input /></a-col>
+                <a-col :span="6">
+                    <p>创建时间</p>
+                    <p>更新时间</p>
+                    <p>只看需要核销&只看被驳回</p>
+                </a-col>
+            </a-row>
+
+
+        </a-layout-content>
     </a-drawer>
 </template>
 <script>
-import { defineComponent,ref, } from 'vue';
+import { defineComponent,ref,reactive } from 'vue';
+
 export default defineComponent({
 
     name: "more_select",  // 筛选条件查询组件
@@ -29,6 +53,12 @@ export default defineComponent({
 
     setup(props, ctx) {
 
+        const SelectData = reactive({
+            product_id_list:ref(undefined),
+            sku_code:ref(undefined),
+            sort_id:ref(undefined)
+        })
+
         console.log(props.data.MoreSelectData)
 
         const handleOk = e => {
@@ -42,7 +72,8 @@ export default defineComponent({
         return{
             props,
             handleOk,
-            onClose
+            onClose,
+            SelectData
         }
     }
 })
