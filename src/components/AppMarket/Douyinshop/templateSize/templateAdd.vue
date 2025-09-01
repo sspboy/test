043d655b52ab158d66cd57b442814627d38b8977
sspboy <sspboy@163.com>
2025-09-01 18:02:04 +0800

    '20250911'

--- src/components/AppMarket/Douyinshop/templateSize/templateAdd.vue
+++ src/components/AppMarket/Douyinshop/templateSize/templateAdd.vue
@@ -31,8 +31,8 @@
         <a-checkbox-group 
             v-model:value="size_add.op_value" 
             name="checkboxgroup" 
-            :options="size_add[size_add.op_name]" 
-            @change="console.log(size_add.op_value)"
+            :options="size_add[size_add.op_name].columns_list" 
+            @click="size_add.checked_columns"
         />
     </div>
     <p>
@@ -79,142 +79,372 @@ setup(props,ctx) {
 
     // 新建尺码详情
     const size_add = reactive({
+
         open:ref(false),
         data:ref(undefined),
+
         // 新建模板类型选择
         add_value:ref('clothing'),
+
         // 模板选项
         template_type:{
             'clothing':'服装',
             'undies':'内衣',
             'shoes':'鞋靴类',
             'children_clothing':'童装',
+            'rings':'戒指',
+            'bracelets':'手镯'
         },
 
-        op_value:ref([]),// 选中的尺码值
+        op_value:ref(['身高(cm)', '体重(cm)', '胸围(cm)', '肩宽(cm)', '腰围(cm)', '臀围(cm)', '袖长(cm)']),// 选中的尺码值
 
         op_name:ref('clothing'),
 
-        clothing:['身高(cm)', '体重(cm)', '胸围(cm)', '肩宽(cm)', '腰围(cm)', '臀围(cm)', '袖长(cm)'],
+        clothing:{
+            columns_list:['身高(cm)', '体重(cm)', '胸围(cm)', '肩宽(cm)', '腰围(cm)', '臀围(cm)', '袖长(cm)'],
+            index_list:['Height', 'Weight', 'Chest_circumference', 'Shoulder_width', 'Waist_circumference', 'Hip_circumference', 'Sleeve_length'],
+        },
 
-        undies:['下胸围(cm)', '胸围差(cm)', '杯罩'],
+        undies:{
+            columns_list:['下胸围(cm)', '胸围差(cm)', '杯罩'],
+            index_list:['Lower_chest_circumference','Chest_circumference_difference','cup_cover']
+        },
 
-        shoes:['脚长(cm)', '脚宽(cm)', '靴筒高(cm)', '靴筒围(cm)', '鞋跟高(cm)'],
+        shoes:{
+            columns_list:['脚长(cm)', '脚宽(cm)', '靴筒高(cm)', '靴筒围(cm)', '鞋跟高(cm)'],
+            index_list:['length','width','height','circumference','Heel_height']
+        },
 
-        children_clothing:['身高(cm)', '体重(斤)', '胸围(cm)'],
+        children_clothing:{
+            columns_list:['身高(cm)', '体重(斤)', '胸围(cm)'],
+            index_list:['Height','Weight','Age_Month']
+        },
+        
+        rings:{
+           columns_list:['戒指内周长(mm)', '戒指内直径(mm)'],
+           index_list:['inner_circumference','inner_diameter']
+        },
 
-        rings:['戒指内周长(mm)', '戒指内直径(mm)'],
-
-        bracelets:['手掌最宽长度(mm)', '手掌最宽周长(mm)'],
+        bracelets:{
+            columns_list:['手掌最宽长度(mm)', '手掌最宽周长(mm)'],
+            index_list:['width_length','circumference_palm']
+        },
 
         // 切换表头方法
         check_colums:(type_name)=>{
+            
+            var r = size_add[type_name].columns_list
+            var d = size_add[type_name].index_list
 
-            var r = size_add[type_name]
+            var list = [
+                {
+                    title: '尺码',
+                    dataIndex: 'size',
+                    align: 'center',
+                    width:80,
+                },
+            ] // 表头
 
-            var list = [] // 表头
-
-            for(let i of r){
+            for(let i=0;i<r.length;i++){
                 var o = {}
-                o.title = ''
-                o.dataIndex = ''
+                o.title = r[i]
+                o.dataIndex = d[i]
                 list.push(o)
             }
 
+            list.push({
+                title: '操作',
+                dataIndex: 'op',
+                align: 'center',
+                width:120,
+            })
+            
+            console.log(type_name)
             console.log(list)
-            // 初始化模板数据
+
             // 服装
             let clothingData = [
+                    {
+                        key: '1',
+                        size: 'S',// 尺码
+                        Height: '160', // 身高(cm)
+                        Weight: '50',//体重(cm)
+                        Chest_circumference: '34',// 胸围(cm)
+                        Shoulder_width:'22',// '肩宽(cm)'
+                        Waist_circumference:'22',// '腰围(cm)'
+                        Hip_circumference:'22', // '臀围(cm)'
+                        Sleeve_length:'22'// '袖长(cm)'
+                    },
+                    {
+                        key: '2',
+                        size: 'M',// 尺码
+                        Height: '160', // 身高(cm)
+                        Weight: '50',//体重(cm)
+                        Chest_circumference: '34',// 胸围(cm)
+                        Shoulder_width:'22',// '肩宽(cm)'
+                        Waist_circumference:'22',// '腰围(cm)'
+                        Hip_circumference:'22', // '臀围(cm)'
+                        Sleeve_length:'22'// '袖长(cm)'
+                    },
+                    {
+                        key: '3',
+                        size: 'L',// 尺码
+                        Height: '160', // 身高(cm)
+                        Weight: '50',//体重(cm)
+                        Chest_circumference: '34',// 胸围(cm)
+                        Shoulder_width:'22',// '肩宽(cm)'
+                        Waist_circumference:'22',// '腰围(cm)'
+                        Hip_circumference:'22', // '臀围(cm)'
+                        Sleeve_length:'22'// '袖长(cm)'
+                    },
+                    {
+                        key: '4',
+                        size: 'XL',// 尺码
+                        Height: '160', // 身高(cm)
+                        Weight: '50',//体重(cm)
+                        Chest_circumference: '34',// 胸围(cm)
+                        Shoulder_width:'22',// '肩宽(cm)'
+                        Waist_circumference:'22',// '腰围(cm)'
+                        Hip_circumference:'22', // '臀围(cm)'
+                        Sleeve_length:'22'// '袖长(cm)'
+                    },
+            ]
+
+            // 内衣
+            let undiesData = [
                 {
                     key: '1',
-                    size: 's',// 尺码
-                    Height: '160', // 身高(cm)
-                    Weight: '50',//体重(cm)
-                    Chest_circumference: '34',// 胸围(cm)
-                    Shoulder_width:'22',// '肩宽(cm)'
-                    Waist_circumference:'22',// '腰围(cm)'
-                    Hip_circumference:'22', // '臀围(cm)'
-                    Sleeve_length:'22'// '袖长(cm)'
+                    size: 'S',// 尺码
+                    Lower_chest_circumference: '160', // 下胸围(cm)
+                    Chest_circumference_difference: '50',//胸围差(cm)
+                    cup_cover: '34',// 罩杯
                 },
                 {
                     key: '2',
-                    size: 's',// 尺码
-                    Height: '160', // 身高(cm)
-                    Weight: '50',//体重(cm)
-                    Chest_circumference: '34',// 胸围(cm)
-                    Shoulder_width:'22',// '肩宽(cm)'
-                    Waist_circumference:'22',// '腰围(cm)'
-                    Hip_circumference:'22', // '臀围(cm)'
-                    Sleeve_length:'22'// '袖长(cm)'
+                    size: 'M',// 尺码
+                    Lower_chest_circumference: '160', // 下胸围(cm)
+                    Chest_circumference_difference: '50',//胸围差(cm)
+                    cup_cover: '34',// 罩杯
+                },
+                {
+                    key: '3',
+                    size: 'X',// 尺码
+                    Lower_chest_circumference: '160', // 下胸围(cm)
+                    Chest_circumference_difference: '50',//胸围差(cm)
+                    cup_cover: '34',// 罩杯
                 },
             ]
             
-            // 内衣
-            let undiesData = []
-            
             // 鞋靴
-            let shoesData = []
+            let shoesData = [
+                {                    
+                    key: '1',
+                    size: 'S',// 尺码
+                    length:'33',// 脚长(cm) 
+                    width:'33',// 脚宽(cm)
+                    height:'33', // 靴筒高(cm)
+                    circumference:'33', // 靴筒围(cm)
+                    Heel_height:'33'// 鞋根高(cm)
+                },
+                {                    
+                    key: '2',
+                    size: 'M',// 尺码
+                    length:'33',// 脚长(cm) 
+                    width:'33',// 脚宽(cm)
+                    height:'33', // 靴筒高(cm)
+                    circumference:'33', // 靴筒围(cm)
+                    Heel_height:'33'// 鞋根高(cm)
+
+                },
+                {                    
+                    key: '3',
+                    size: 'L',// 尺码
+                    length:'33',// 脚长(cm) 
+                    width:'33',// 脚宽(cm)
+                    height:'33', // 靴筒高(cm)
+                    circumference:'33', // 靴筒围(cm)
+                    Heel_height:'33'// 鞋根高(cm)
+
+                },
+            ]
             
             // 童装
-            let children_clothingData=[]
+            let children_clothingData=[
+                {                    
+                    key: '1',
+                    size: 'S',// 尺码
+                    Height:'',// 身高(cm) 
+                    Weight:'',// 体重(cm)
+                    Age_Month:'', // 年龄/月龄
+                },
+                {                    
+                    key: '2',
+                    size: 'M',// 尺码
+                    Height:'',// 身高(cm) 
+                    Weight:'',// 体重(cm)
+                    Age_Month:'', // 年龄/月龄
+                },
+                {                    
+                    key: '3',
+                    size: 'L',// 尺码
+                    Height:'',// 身高(cm) 
+                    Weight:'',// 体重(cm)
+                    Age_Month:'', // 年龄/月龄
+                },
+            ]
             
             // 戒指
-            let ringsData = []
+            let ringsData = [
+                {
+                    key: '1',
+                    size: '1号',// 尺码
+                    inner_circumference:'',// 内周长(mm) 
+                    inner_diameter:'',// 内直径(cm)
+                },
+                {
+                    key: '2',
+                    size: '2号',// 尺码
+                    inner_circumference:'',// 内周长(mm) 
+                    inner_diameter:'',// 内直径(cm)
+                },
+                {
+                    key: '3',
+                    size: '3号',// 尺码
+                    inner_circumference:'',// 内周长(mm) 
+                    inner_diameter:'',// 内直径(cm)
+                },
+            ]
             
             // 手镯
-            let bracelets = []
+            let bracelets = [
+                {
+                    key: '1',
+                    size: '3号',// 尺码
+                    width_length:'',// 手掌最宽长度(mm) 
+                    circumference_palm:'',// 手掌最宽周长(mm)
+                },
+                {
+                    key: '2',
+                    size: '3号',// 尺码
+                    width_length:'',// 手掌最宽长度(mm) 
+                    circumference_palm:'',// 手掌最宽周长(mm)
+                },
+                {
+                    key: '3',
+                    size: '3号',// 尺码
+                    width_length:'',// 手掌最宽长度(mm) 
+                    circumference_palm:'',// 手掌最宽周长(mm)
+                },
+            ]
+            
+            // 初始化模板数据开始
+            size_add.columns = list
+            if(type_name == 'clothing'){
+                size_add.data = clothingData
+            }else if(type_name == 'undies'){
+                size_add.data = undiesData
+            }else if(type_name == 'shoes'){
+                size_add.data = shoesData
+            }else if(type_name == 'children_clothing'){
+                size_add.data = children_clothingData
+            }else if(type_name == 'rings'){
+                size_add.data = ringsData
+            }else if(type_name == 'bracelets'){
+                size_add.data = bracelets
+            }
 
         },
         // 表头数据
         columns:ref([
             {
-                title: '尺码',
-                dataIndex: 'size',
-                align: 'center',
-                width:80,
-            }, 
-            {
-                title: 'Name',
-                dataIndex: 'name',
-
-            }, 
-            {
-                title: 'Cash Assets',
-                dataIndex: 'money',
-            }, 
-            {
-                title: 'Address',
-                dataIndex: 'address',
+                "title": "尺码",
+                "dataIndex": "size",
+                "align": "center",
+                "width": 80
             },
             {
-                title: '操作',
-                dataIndex: 'op',
-                align: 'center',
-                width:120,
+                "title": "身高(cm)",
+                "dataIndex": "Height"
+            },
+            {
+                "title": "体重(cm)",
+                "dataIndex": "Weight"
+            },
+            {
+                "title": "胸围(cm)",
+                "dataIndex": "Chest_circumference"
+            },
+            {
+                "title": "肩宽(cm)",
+                "dataIndex": "Shoulder_width"
+            },
+            {
+                "title": "腰围(cm)",
+                "dataIndex": "Waist_circumference"
+            },
+            {
+                "title": "臀围(cm)",
+                "dataIndex": "Hip_circumference"
+            },
+            {
+                "title": "袖长(cm)",
+                "dataIndex": "Sleeve_length"
+            },
+            {
+                "title": "操作",
+                "dataIndex": "op",
+                "align": "center",
+                "width": 120
             }
         ]),
 
         // 模板数据
         data:ref([
-            {
-            key: '1',
-            size: 'x',
-            name: 'John Brown',
-            money: '￥300,000.00',
-            address: 'New York No. 1 Lake Park',
-            }, {
-            key: '2',
-                size: 'x',
-                name: 'Jim Green',
-                money: '￥1,256,000.00',
-                address: 'London No. 1 Lake Park',
-            }, {
-                key: '3',
-                size: 'x',
-                name: 'Joe Black',
-                money: '￥120,000.00',
-                address: 'Sidney No. 1 Lake Park',
-        }]),
+                    {
+                        key: '1',
+                        size: 'S',// 尺码
+                        Height: '160', // 身高(cm)
+                        Weight: '50',//体重(cm)
+                        Chest_circumference: '34',// 胸围(cm)
+                        Shoulder_width:'22',// '肩宽(cm)'
+                        Waist_circumference:'22',// '腰围(cm)'
+                        Hip_circumference:'22', // '臀围(cm)'
+                        Sleeve_length:'22'// '袖长(cm)'
+                    },
+                    {
+                        key: '2',
+                        size: 'M',// 尺码
+                        Height: '160', // 身高(cm)
+                        Weight: '50',//体重(cm)
+                        Chest_circumference: '34',// 胸围(cm)
+                        Shoulder_width:'22',// '肩宽(cm)'
+                        Waist_circumference:'22',// '腰围(cm)'
+                        Hip_circumference:'22', // '臀围(cm)'
+                        Sleeve_length:'22'// '袖长(cm)'
+                    },
+                    {
+                        key: '3',
+                        size: 'L',// 尺码
+                        Height: '160', // 身高(cm)
+                        Weight: '50',//体重(cm)
+                        Chest_circumference: '34',// 胸围(cm)
+                        Shoulder_width:'22',// '肩宽(cm)'
+                        Waist_circumference:'22',// '腰围(cm)'
+                        Hip_circumference:'22', // '臀围(cm)'
+                        Sleeve_length:'22'// '袖长(cm)'
+                    },
+                    {
+                        key: '4',
+                        size: 'XL',// 尺码
+                        Height: '160', // 身高(cm)
+                        Weight: '50',//体重(cm)
+                        Chest_circumference: '34',// 胸围(cm)
+                        Shoulder_width:'22',// '肩宽(cm)'
+                        Waist_circumference:'22',// '腰围(cm)'
+                        Hip_circumference:'22', // '臀围(cm)'
+                        Sleeve_length:'22'// '袖长(cm)'
+                    },
+            ]),
 
         play:()=>{
             size_add.open = true
@@ -225,9 +455,13 @@ setup(props,ctx) {
         },
         change:()=>{
             
-            size_add.op_name = size_add.add_value// 表头多选项目切换
-
-            size_add.check_colums(size_add.add_value) // 表格切换
+            size_add.op_name = size_add.add_value;      // 表头多选项目切换
+            size_add.op_value = size_add[size_add.add_value].columns_list
+            size_add.check_colums(size_add.add_value)   // 表格切换
+        },
+        checked_columns:(e)=>{
+            console.log(e.target.value)
+            console.log(size_add.columns)
 
         }
     })
