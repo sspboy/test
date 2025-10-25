6f4b9336aa722728256c64b47536005456a60dbc
sspboy <sspboy@163.com>
2025-10-25 17:59:27 +0800

    '20251025'

--- src/components/AppMarket/Douyinshop/ProductList/selectImg.vue
+++ src/components/AppMarket/Douyinshop/ProductList/selectImg.vue
@@ -17,36 +17,38 @@
                 <a-col :span="18">
                     <!--图片上传按钮-->
                     <a-space>
+
                         <h3 style="margin: 4px 0 0 0;">选择素材</h3>
+
+                        <!--面包屑导航-->
+                        <div style="margin: 8px 0 0 20px;"> 
+                            <a-breadcrumb>
+
+                                    <a-breadcrumb-item v-for=" item in PAGEDATA.BreadCrumb" href="#">
+                                        <FolderOutlined />
+                                        <span @click="console.log('加载当前文件夹素材列表', item.folder_id)" class="font_size_12">
+                                            {{ item.folder_name }}
+                                        </span>
+                                    </a-breadcrumb-item>
+                            </a-breadcrumb>
+
+                        </div>
+
                     </a-space>
+
                 </a-col>
 
                 <a-col :span="6">
                     <!--图片上传按钮-->
-                    <a-space>
+                    <a-space :size="28">
                         <!--本地上传图片 按钮-->
                         <a-button type="primary" @click="showChildrenDrawer">本地上传图片</a-button>
                         <!--网络地址上传图片 按钮-->
                         <a-button @click="showChildnetDrawer">网络地址上传图片</a-button>
+                        <!--关闭窗口-->
+                        <CloseOutlined @click="onClose" />
                     </a-space>
-                    <!--面包屑导航-->
-                    <!-- <div style="margin: 6px 0 0 0;"> 
-                        <a-breadcrumb>
-                            <a-breadcrumb-item href="">
-                                <FolderOutlined />
-                                <span>主目录</span>
-                            </a-breadcrumb-item>
-
-                            <a-breadcrumb-item href="">
-                                <UserOutlined />
-                                <span>Application List</span>
-                            </a-breadcrumb-item>
-
-                            <a-breadcrumb-item>
-                                Application
-                            </a-breadcrumb-item>
-                        </a-breadcrumb>
-                    </div> -->
+                    
                 </a-col>
             </a-row>
 
@@ -68,11 +70,12 @@
                         v-model:selectedKeys="selectedKeys"
                         :load-data="onLoadData"
                         :tree-data="treeData"
+                        @select="handleSelect"
                         show-icon
                     >
-                    <template #icon="{ key, selected }">
-                        <FolderOutlined />
-                    </template>
+                        <template #icon="{ key, selected }">
+                            <FolderOutlined />
+                        </template>
                     </a-tree>
 
                 </a-layout-sider>
@@ -86,63 +89,65 @@
                     <!-- 列表 不为空状态 -->
                     <a-checkbox-group  v-model:value="Material_Images.check_value.value" style="width: 100%;height: 100%;">
 
-                        <a-layout-content  class="contentStyle">
+                        <a-layout-content class="contentStyle">
 
                             <div style="text-align: left;padding: 10px;height: 100px;margin: 0 0 0 12px;">
                                 已选择图片：
                             </div>
 
                             <!-- 列表 为空状态 -->
+                            <div style="height:650px;overflow-x: hidden;overflow-y: auto;">
+                                <a-list 
+                                    :grid="{ gutter: 6, column: 5 }" 
+                                    :data-source="PAGEDATA.datalist"
+                                    :loading="PAGEDATA.loading"
+                                >
 
-                            <a-list 
-                                :grid="{ gutter: 6, column: 5 }" 
-                                :data-source="PAGEDATA.datalist"
-                                :loading="PAGEDATA.loading"
-                            >
+                                        <template #renderItem="{ item }">
+                                            
 
-                                    <template #renderItem="{ item }">
-                                        
+                                            <a-list-item>
 
-                                        <a-list-item>
+                                                    <!--图片文件 显示方式-->
+                                                    <div v-if="item.material_type == 'photo'" style="border: 1px solid silver; border-radius: 6px;padding: 4px;">
 
-                                                <!--图片文件 显示方式-->
-                                                <div v-if="item.material_type == 'photo'" style="border: 1px solid silver; border-radius: 6px;padding: 4px;">
+                                                        <a-image
+                                                            width="200"
+                                                            :src="item.byte_url"
+                                                        />
+                                                        <p style="padding: 2px;margin: 4px 0 0 0;width: 140px;height: 28px;overflow: hidden;text-align: left;">
+                                                            <a-checkbox :value="item.material_id">{{ item.materil_name }}</a-checkbox>
+                                                        </p>
 
-                                                    <a-image
-                                                        width="200"
-                                                        :src="item.byte_url"
-                                                    />
-                                                    <p style="padding: 2px;margin: 4px 0 0 0;width: 140px;height: 28px;overflow: hidden;text-align: left;">
-                                                        <a-checkbox :value="item.material_id">{{ item.materil_name }}</a-checkbox>
-                                                    </p>
+                                                    </div>
 
-                                                </div>
-
-                                                <!--视频 显示方式-->
-                                                <div v-if="item.material_type == 'video'" style="border: 1px solid silver; border-radius: 6px;padding: 4px;">
-                                                    <a-image
-                                                        width="200"
-                                                        :src="item.video_info.video_cover_url"
-                                                    />
-                                                    <p style="padding: 2px;margin: 4px 0 0 0;width: 90%;height: 28px;overflow: hidden;text-align: left;">
-                                                        <a-checkbox :value="item.material_id">{{ item.materil_name }}</a-checkbox>
-                                                    </p>
-                                                </div>
+                                                    <!--视频 显示方式-->
+                                                    <div v-if="item.material_type == 'video'" style="border: 1px solid silver; border-radius: 6px;padding: 4px;">
+                                                        <a-image
+                                                            width="200"
+                                                            :src="item.video_info.video_cover_url"
+                                                        />
+                                                        <p style="padding: 2px;margin: 4px 0 0 0;width: 90%;height: 28px;overflow: hidden;text-align: left;">
+                                                            <a-checkbox :value="item.material_id">{{ item.materil_name }}</a-checkbox>
+                                                        </p>
+                                                    </div>
 
 
 
-                                        </a-list-item>
-                                    </template>
-                                                                        
+                                            </a-list-item>
+                                        </template>
+                                                                            
 
-                            </a-list>
+                                </a-list>
+                            </div>
 
                         </a-layout-content>
+
                     </a-checkbox-group>
 
                     <!-- 翻页 -->
                     <a-layout-footer class="footerStyle">          
-                        <nav_pagination :fandata="PAGEDATA" v-on:complete="console.log('hehe')"/>
+                        <nav_pagination :fandata="PAGEDATA" v-on:complete="page_turning"/>
                     </a-layout-footer>
                 </a-layout>
             </a-layout>
@@ -181,9 +186,10 @@
 
 <script>
 import { defineComponent,ref,reactive,onMounted,h,watch } from 'vue';
-import { FolderOutlined,HomeOutlined,UserOutlined} from '@ant-design/icons-vue';
+import { FolderOutlined,HomeOutlined,UserOutlined,CloseOutlined } from '@ant-design/icons-vue';
 import * as TOOL from '@/assets/JS_Model/tool';
 import * as utils from '@/assets/JS_Model/public_model';
+import axios from "axios";
 
 // 组件引用=====开始
 import nav_pagination from "@/components/nav_pagination.vue";
@@ -194,30 +200,41 @@ export default defineComponent({
     FolderOutlined,
     UserOutlined,
     HomeOutlined,
+    CloseOutlined,
     nav_pagination, // 翻页组件
    },
     props: {
         data:{typr:Object}
     },
     setup(props,ctx) {
-
+        
+        // 数据配置
         const PAGEDATA = reactive({
             total_number:ref(0),            // 图片列表内容总数
             datalist:ref([]),               // 素材列表数据
             loading:ref(true),              // 素材列表加载状态
             f_id:0,                         // 根目录id
             List_conditions:reactive({      // 默认查询配置
+                "folder_id":0,              // 当前文件夹id
                 "page":1,                   // 当前页面
                 "size":10,                  // 显示数量
-                "total":0                   // 总页数
             }),
+
+            childrenDrawer:ref(false),// 本地上传图片-抽屉状态
+            childnetDrawer:ref(false),// 网络上传图片-抽屉状态
+            BreadCrumb:ref([// 面包屑导航
+                {
+                    "folder_name":"素材库",
+                    "folder_id":0
+                }
+            ])
         })
 
         const tool = new TOOL.TOOL()            // 工具方法
         const API = new utils.A_Patch()         // 请求接口地址合集
 
-        const childrenDrawer = ref(false); // 本地上传抽屉状态
-        const childnetDrawer = ref(false);// 网络上传抽屉状态
+        const childrenDrawer = ref(false);      // 本地上传抽屉状态
+        const childnetDrawer = ref(false);      // 网络上传抽屉状态
 
         // 关闭选择素材抽屉
         const onClose = () => {
@@ -261,15 +278,29 @@ export default defineComponent({
 
                     // 符合条件文件夹数量
                     var total_child_material_num = res.data.data.folder_info.total_child_material_num;
-                    console.log(total_child_material_num)
+                    // console.log(total_child_material_num)
                     PAGEDATA.total_number = total_child_material_num
 
                     // 添加菜单名称和id
                     folder_info_list.forEach((obj, idx)=>{
+                        
+                        // console.log(obj.operate_status)
 
+                        // parent_folder_id 父文件夹id
                         obj.title = obj.folder_name;
-                        obj.key = obj.folder_id;
-                        obj.isLeaf = true;
+                        obj.key = obj.folder_id;                      
+                        obj.isLeaf = true; 
+
+                        // 面包屑
+                        obj.breadcrumb = [
+                            {
+                            "folder_name":"素材库",
+                            "folder_id":0
+                            },{
+                            "folder_name":obj.folder_name,
+                            "folder_id":obj.folder_id
+                            }
+                        ]
 
                         tool.Http_.post(API.AppSrtoreAPI.material.getfolder,{
                             "folder_id":obj.folder_id,// 文件夹id
@@ -285,11 +316,7 @@ export default defineComponent({
                     })
 
                     // 初始化图片列表
-                    child_material.forEach((obj, idx)=>{
-                        // console.log(obj)
-                        obj.title=obj.materil_name; // 图片名称
-                        PAGEDATA.datalist.push(obj); // 添加到图片列表
-                    })
+                    PAGEDATA.datalist = [...child_material]
 
                 })
 
@@ -317,10 +344,6 @@ export default defineComponent({
 
             },
 
-
-            // 根据id_查询素材文件+素材图片
-
-
             // 素材图片列表
             data_img_list:ref([]),
             
@@ -340,8 +363,8 @@ export default defineComponent({
         }
 
 
-        const expandedKeys = ref([]);
-        const selectedKeys = ref([]);
+        const expandedKeys = ref([]); // 展开指定的树节点
+        const selectedKeys = ref([]); // 选中的节点树
         const treeData = ref([]);
 
         // 初始化素材菜单
@@ -351,15 +374,23 @@ export default defineComponent({
         const onLoadData = treeNode => {
             return new Promise(resolve => {
 
+                // 如果存在子菜单 跳过
+                if (treeNode.dataRef.children) {
+                    resolve();
+                    return;
+                }
+
                 var F_if = treeNode.key
 
-                console.log('加载树', F_if)
+                // console.log('加载树', F_if)
 
                 // 请求文件夹信息
                 tool.Http_.post(API.AppSrtoreAPI.material.getfolder,{
+
                     "folder_id":F_if,// 文件夹id
                     "page_num":1,
-                    "page_size":100
+                    "page_size":10
+
                 }).then((res)=>{
 
                     // 当前文件夹id
@@ -380,16 +411,25 @@ export default defineComponent({
 
                     // 数量=0:禁用子菜单
                     if(chile_folder_number == 0){
-                        console.log('子文件夹为空', child_folder_list)
+                        // console.log('子文件夹为空', child_folder_list)
                         treeNode.isLeaf = true;
                         treeData.value = [...treeData.value];
                         resolve();
                     }else {// 数量!=0：加载子菜单
+
                         console.log('子文件夹',child_folder_list)
+
                         // 添加菜单名称和id
                         child_folder_list.forEach((obj, idx)=>{
+                            // 面包屑
+                            var breadcrumb = treeNode.dataRef.breadcrumb;
                             obj.title = obj.folder_name;
                             obj.key = obj.folder_id;
+                            obj.breadcrumb = [...breadcrumb]
+                            obj.breadcrumb.push({
+                                "folder_id":obj.folder_id,
+                                "folder_name":obj.folder_name
+                            })
                         })
                         treeNode.dataRef.children = child_folder_list;
                         treeData.value = [...treeData.value];
@@ -407,47 +447,17 @@ export default defineComponent({
 
                 })
 
-                // 如果存在子菜单 跳过
-                if (treeNode.dataRef.children) {
-                    resolve();
-                    return;
-                }
 
 
-                // 如果没有子菜单 则添加子菜单
-                // setTimeout(() => {
-
-                //     treeNode.dataRef.children = [
-                //         // {
-                //         //     title: 'Child Node',
-                //         //     key: `${treeNode.eventKey}-0`,
-                //         // },
-                //         // {
-                //         //     title: 'Child Node',
-                //         //     key: `${treeNode.eventKey}-1`,
-                //         // },
-                //     ];
-
-                //     treeData.value = [...treeData.value];
-
-                //     resolve();
-
-                // }, 1000);
             });
         };
         
-        // 点击菜单===》 加载图片列表
-        watch(selectedKeys, () => {
+        const handleSelect = (keys,event) =>{
+            console.log(keys)
+            console.log(event.node)
+            PAGEDATA.BreadCrumb = [...event.node.breadcrumb]
+        }
 
-            console.log('文件夹id', selectedKeys.value[0]);
-
-            console.log('加载素材列表')
-
-            console.log('翻页组件')
-
-            // PAGEDATA.total_number = 10
-
-        });
 
         // 请求商品列表接口数据
         const loadproductData = async(data) => {
@@ -455,14 +465,21 @@ export default defineComponent({
             PAGEDATA.loading = true;
 
             // 请求商品接口
-            const res = await axios.post(API.AppSrtoreAPI.dou_product.list, data)
+            const res = await axios.post(API.AppSrtoreAPI.material.getfolder, data)
+            // console.log(res)
 
             var res_data = res.data.data;
-            var res_list = res_data.data;
-            var total = res_data.total;
+
+            var folder_info = res_data.folder_info;
+
+            // 素材列表
+            var child_material = folder_info.child_material;
+
+            // 素材总数 total_child_material_num
+            var total = folder_info.total_child_material_num;
 
             // 请求数据为空
-            if(res_list.length == 0){
+            if(child_material.length == 0){
                 PAGEDATA.justify = 'center';
                 PAGEDATA.align = 'center';
                 PAGEDATA.loading = false;
@@ -474,7 +491,7 @@ export default defineComponent({
                     PAGEDATA.justify = 'start';
                     PAGEDATA.align = 'start';
                     // 请求数据不为空
-                    PAGEDATA.datalist = res_list;
+                    PAGEDATA.datalist = [...child_material];
                     PAGEDATA.total_number = total;
                 }, 1000);
             }
@@ -483,19 +500,22 @@ export default defineComponent({
         // 【翻页-组件 回调方法】========================================开始
         // 翻页查询条件
         const navData=ref({
-            "page":1,
-            "size":10,
+            "folder_id":0,//默认文件夹id
+            "page_num":1,
+            "page_size":10,
         })
 
         const page_turning = (data)=>{
+
+            console.log(data)
 
             PAGEDATA.justify = 'flex-start';
             
             PAGEDATA.align = 'flex-start';
 
-            navData.value.page = data.page;
+            navData.value.page_num = data.page;
             
-            navData.value.size = data.page_size;
+            navData.value.page_size = data.page_size;
 
             loadproductData(navData.value)
 
@@ -517,13 +537,15 @@ export default defineComponent({
             selectedKeys,
             treeData,
             onLoadData,
+            page_turning,
+            handleSelect
         }
     }
 })
 </script>
 <style scoped>
 .headerStyle{text-align: left;background-color: #fff;height: 64px;line-height: 64px;}
-.contentStyle{text-align: center;background-color: #fff;}
+.contentStyle{text-align: center;background-color: #fff;height: 100%;}
 .siderStyle{background-color: #fff;overflow: auto;padding: 10px 10px 10px 0;width: 400px;}
 .footerStyle{text-align: center;background-color: #fff;}
 </style>
